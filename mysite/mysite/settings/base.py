3523fd2c59fd024c4bb3888a3e6723d61b6009c5
Rob8624 <robert_melen@yahoo.co.uk>
2023-12-28 01:57:22 +0000

    added DB

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -245,8 +245,7 @@ INTERNAL_IPS = [
     "127.0.0.1", "172.17.0.1"
 ]
 
-# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-NPM_BIN_PATH = "/usr/local/bin/npm"
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
