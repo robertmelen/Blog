c7a2fdb01ecc35d9c097edba03879ee78e7d0e72
Rob8624 <robert_melen@yahoo.co.uk>
2023-12-30 15:35:08 +0000

    new

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -236,7 +236,7 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 
