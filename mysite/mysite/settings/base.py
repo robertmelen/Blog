4997122652c5e207769bc7f5ccd997f0af17fcd6
Rob8624 <robert_melen@yahoo.co.uk>
2023-12-30 02:15:26 +0000

    removed whitemnoise static setting

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -290,7 +290,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 AWS_S3_FILE_OVERWRITE = False
