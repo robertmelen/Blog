17c224be751022fed501ac06954747fb67f314a8
Rob8624 <robert_melen@yahoo.co.uk>
2023-12-30 01:33:02 +0000

    changed whitenoise middleware order

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -83,12 +83,13 @@ MIDDLEWARE = [
     
     "django.middleware.common.CommonMiddleware",
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
     'home.middleware.VisitorMiddleware',
     
