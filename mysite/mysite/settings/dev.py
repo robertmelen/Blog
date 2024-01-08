05fe94f25eab422a082855cff682cb54495373db
Rob8624 <robert_melen@yahoo.co.uk>
2024-01-08 02:13:25 +0000

    moved SK to env

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure---w*5k%q(t0_l%wg-nws!l1+x^6#$w*@s51&)tu%n-#s@zunj3"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 
